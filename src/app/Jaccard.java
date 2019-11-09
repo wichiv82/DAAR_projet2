6fe9b4ed46db46fd56d50d1479d5055d408e60f6
3873896 <3873896@ppti-14-502-15.ufr-info-p6.jussieu.fr>
2019-11-09 12:20:47 +0100

    CLoseness et début de Betwenness

--- src/app/Jaccard.java
+++ src/app/Jaccard.java
@@ -3,10 +3,10 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Jaccard {
-	
 	
 	public static HashMap<String, Integer> getOccurences(String filename){
 		HashMap<String, Integer> occurences = new HashMap<String, Integer>();
@@ -50,5 +50,77 @@ public class Jaccard {
 			return 0;
 		
 		return numerateur/denominateur;
+	}
+	
+	
+	public static double closeness(String doc, ArrayList<String> documents) {
+		double denominateur = 0;
+		int nbDocuments = documents.size();
+		
+		HashMap<String, Integer> doc_occurences = getOccurences(doc);
+		
+		for(String file : documents) {
+			if(doc.equals(file))
+				nbDocuments--;
+			else
+				denominateur += distanceJaccard(doc_occurences, getOccurences(file));
+		}
+		
+		if(denominateur == 0)
+			return 0;
+		
+		return (nbDocuments) / denominateur;
+	}
+	
+	/**MAYBE TO DO**/
+	public static double betweenness(ArrayList<String> doc, double edgeThreshold) {
+		double denominateur = 0;
+		
+		int[][] paths=new int[doc.size()][doc.size()];
+		for (int i=0;i<paths.length;i++) for (int j=0;j<paths.length;j++) paths[i][j]=i;
+		
+		double[][] dist=new double[doc.size()][doc.size()];
+
+	    for (int i=0;i<paths.length;i++) {
+	    	for (int j=0;j<paths.length;j++) {
+	    		if (i==j) {
+	    			dist[i][i]=0; 
+	    			continue;
+	    		}
+	        
+	    		HashMap<String, Integer> d1 = getOccurences(doc.get(i));
+	    		HashMap<String, Integer> d2 = getOccurences(doc.get(j));
+	    		double d = distanceJaccard(d1, d2);
+	        
+	    		if (d <= edgeThreshold)
+	    			dist[i][j] = d; 
+	    		else 
+	    			dist[i][j] = Double.POSITIVE_INFINITY;
+	        
+	    		paths[i][j]=j;
+	    	}
+	    }
+	    
+	    for (int k=0;k<paths.length;k++) {
+	    	for (int i=0;i<paths.length;i++) {
+	    		for (int j=0;j<paths.length;j++) {
+	    			if (dist[i][j]>dist[i][k] + dist[k][j]){
+	    				dist[i][j]=dist[i][k] + dist[k][j];
+	    				paths[i][j]=paths[i][k];
+
+	    			}
+	    		}
+	    	}
+	    }	
+		
+		if(denominateur == 0)
+			return 0;
+		
+		return (doc.size()-1) / denominateur; // FAUX, RETOURNER LA FORMULE DU FUTUR AVEC LES PPC :
+		
+		// B(v) = Somme(#ppc (s,t passent par v) / #ppc (s,t) ) 
+		// avec s != t € V
+		// 		s != v
+		//		t != v
 	}
 }
